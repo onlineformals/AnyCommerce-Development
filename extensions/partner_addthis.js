bc62b0c890beb7800f898026119e9cc741fa220b
clintoncontreras <ClintonC@zoovy.com>
2014-04-04 17:35:33 -0700

    Fixed addThis extension

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -78,6 +78,46 @@ var partner_addthis = function(_app) {
 				onError : function() {
 					_app.u.dump('BEGIN _app.ext.partner_addthis.callbacks.init.onError');
 				}
+			},
+ 			
+ 			
+ 			startExtension : {
+ 				onSuccess : function(){
+ 					dump("BEGIN _app.ext.partner_addthis.callbacks.startExtension.onSuccess");
+ 					var scriptPath = (document.location.protocol == 'https:' ? 'https:' : 'http:')+'//s7.addthis.com/js/250/addthis_widget.js';
+ 					if(_app.ext.partner_addthis.vars.addthis_config.username && _app.ext.partner_addthis.vars.addthis_config.username !== ""){
+ 						scriptPath+= '#username='+_app.ext.partner_addthis.vars.addthis_config.username+'&domready';
+ 						}
+ 					else {
+ 						scriptPath += '#domready';
+ 						}
+ 					_app.u.loadScript(scriptPath);
+ 					
+ 					//This is an example of how to add an addthis toolbox to a product page
+ 					_app.templates.productTemplate.on('complete.formals',function(event,$context,infoObj){
+ 						var $context = $(_app.u.jqSelector('#',infoObj.parentID));
+ 						var $toolbox = $('.socialLinks', $context);
+ 						if($toolbox.hasClass('addThisRendered')){
+ 							//Already rendered, don't do it again.
+ 							}
+ 						else {
+ 							$toolbox.addClass('addThisRendered').append(
+ 									'<div id="socialLinks" class="addthis_toolbox addthis_default_style">'
+ 								+		'<a class="addthis_button_preferred_1"></a>'
+ 								+		'<a class="addthis_button_preferred_2"></a>'
+ 								+		'<a class="addthis_button_preferred_3"></a>'
+ 								+		'<a class="addthis_button_preferred_4"></a>'
+ 								+		'<a class="addthis_button_compact"></a>'
+ 								+	'</div>');
+ 							
+ 							_app.ext.partner_addthis.u.toolbox($toolbox, infoObj);
+ 							}
+ 						});
+ 					return true;
+ 				},
+ 				onError : function() {
+ 					_app.u.dump('BEGIN _app.ext.partner_addthis.callbacks.startExtension.onError');
+ 				}
 			}
 		},
 		
