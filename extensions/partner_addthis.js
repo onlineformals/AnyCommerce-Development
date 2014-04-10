1a20d53c1a418e5f8304ac395958bc0416bafc37
clintoncontreras <ClintonC@zoovy.com>
2014-04-10 11:05:16 -0700

    Updated add this social media icons

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -104,13 +104,13 @@ var partner_addthis = function(_app) {
  							}
  						else {
  							$toolbox.addClass('addThisRendered').append(
- 									'<div id="socialLinks" class="addthis_toolbox addthis_default_style">'
- 								+		'<a class="addthis_button_preferred_1"></a>'
- 								+		'<a class="addthis_button_preferred_2"></a>'
- 								+		'<a class="addthis_button_preferred_3"></a>'
- 								+		'<a class="addthis_button_preferred_4"></a>'
- 								+		'<a class="addthis_button_compact"></a>'
- 								+	'</div>');
+ 									'<div id="socialLinks" class="addthis_toolbox addthis_default_style addthis_32x32_style">'
+							+		'<a class="addthis_button_preferred_1"></a>'
+							+		'<a class="addthis_button_preferred_2"></a>'
+							+		'<a class="addthis_button_preferred_3"></a>'
+							+		'<a class="addthis_button_preferred_4"></a>'
+							+		'<a class="addthis_button_compact"></a>'
+							+		'</div>');
  							
  							_app.ext.partner_addthis.u.toolbox($toolbox, infoObj);
  							}
