717641fc23bedd6c4c8bc72935128ef75371de6f
clintoncontreras <ClintonC@zoovy.com>
2014-05-12 16:48:55 -0700

    Fixed addThis again + versioning

--- extensions/partner_addthis.js
+++ extensions/partner_addthis.js
@@ -101,7 +101,7 @@ var partner_addthis = function(_app) {
 							}
 						else {
 							$toolbox.addClass('addThisRendered').append(
-									'<div id="socialLinks" class="addthis_toolbox addthis_default_style">'
+									'<div id="socialLinks" class="addthis_default_style addthis_32x32_style">'
 								+		'<a class="addthis_button_preferred_1"></a>'
 								+		'<a class="addthis_button_preferred_2"></a>'
 								+		'<a class="addthis_button_preferred_3"></a>'
